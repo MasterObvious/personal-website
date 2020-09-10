ba3614d6c192ed12847d18802b6dd9ed2dea4e01
Tim Lazarus <me@timlazarus.com>
2020-09-10 23:57:56 +0100

    Improve hero layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,8 +6,11 @@ import "./styles/Hero.css";
 
 export default function Hero() {
   return (
-    <Flex className="hero-container">
-      <Text className="hero-text" fontSize={{ base: "4xl", sm: "6xl" }}>
+    <Flex className="hero-container" justify="flex-start" w="85%">
+      <Text
+        className="hero-text"
+        fontSize={{ base: "4xl", sm: "5xl", md: "6xl" }}
+      >
         I'm Tim Lazarus
       </Text>
     </Flex>
