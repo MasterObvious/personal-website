2c64e226299f6c8843204a53ae5084fd382c71e1
Tim Lazarus <me@timlazarus.com>
2020-09-30 18:30:12 +0100

    Add basic hero

--- src/Layouts/Homepage.js
+++ src/Layouts/Homepage.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "../components/Header.js";
+import Hero from "../components/Hero.js";
 
 import "./styles/Homepage.css";
 
@@ -8,6 +9,7 @@ export default function Homepage() {
   return (
     <div className="main-container">
       <Header />
+      <Hero />
     </div>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,21 @@
 import React from "react";
 
+import macbook from "../images/macbook.png";
+
+import "./styles/Hero.css";
+
 export default function Hero() {
-  return <div></div>;
+  return (
+    <div className="hero-container">
+      <div className="hero-text-container">
+        <div className="hero-text-padding">
+          <div>I'm Tim Lazarus.</div>
+          <div className="hero-subtext">A software engineer.</div>
+        </div>
+      </div>
+      <div className="hero-image-container">
+        <img className="hero-image" src={macbook} alt="macbook"></img>
+      </div>
+    </div>
+  );
 }
