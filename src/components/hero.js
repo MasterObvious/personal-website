8c71f0524555fc43fa81249f70764793d6bdd43e
Tim Lazarus <me@timlazarus.com>
2021-03-28 16:01:46 +0100

    Add macbook component

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,13 +3,20 @@ import React from "react"
 import ViewportDiv from "./ViewportDiv"
 
 import * as styles from "./styles/hero.module.css"
+import macbook from "../images/macbook.png"
 
 export default function Hero() {
   return (
     <ViewportDiv>
       <div className={styles.container}>
-        <h1 className={styles.title}>I'm Tim Lazarus.</h1>
-        <h2 className={styles.subtitle}>A software engineer.</h2>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>I'm Tim Lazarus.</h1>
+          <h2 className={styles.subtitle}>A software engineer.</h2>
+        </div>
+        <div className={styles.imageContainer}>
+          <img src={macbook} />
+          <div className={styles.screenOverlay}></div>
+        </div>
       </div>
     </ViewportDiv>
   )
