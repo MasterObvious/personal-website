0b5bcd29487df63361e04bb613ccb7e198f785af
Tim Lazarus <me@timlazarus.com>
2021-03-28 18:25:50 +0100

    Optimise site for mobile

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism"
@@ -10,34 +10,27 @@ import * as styles from "./styles/hero.module.css"
 
 export default function Hero() {
   const codeString = `fn main() {
-  let x = 5;
-  let y = x + 6;
-  println!("Hello World!");
-  let x = 5;
-  let y = x + 6;
-  println!("Hello World!");
-  let x = 5;
-  let y = x + 6;
-  println!("Hello World!");
-  let x = 5;
-  let y = x + 6;
-  println!("Hello World!");
-  let x = 5;
-  let y = x + 6;
-  println!("Hello World!");
-  let x = 5;
-  let y = x + 6;
-  println!("Hello World!");
-  let x = 5;
-  let y = x + 6;
-  println!("Hello World!");
-  let x = 5;
-  let y = x + 6;
-  println!("Hello World!");
-  let x = 5;
-  let y = x + 6;
-  println!("Hello World!");
+  println!("Welcome!");
 }`
+
+  const [subIndex, setSubIndex] = useState(0)
+  const [showBlink, setShowBlink] = useState(true)
+
+  useEffect(() => {
+    let timeout = null
+
+    if (subIndex === codeString.length) {
+      timeout = setTimeout(() => {
+        setShowBlink(prev => !prev)
+      }, 500)
+    } else {
+      timeout = setTimeout(() => {
+        setSubIndex(prev => prev + 1)
+      }, 50)
+    }
+
+    return () => clearTimeout(timeout)
+  }, [subIndex, showBlink])
 
   return (
     <ViewportDiv>
@@ -53,7 +46,7 @@ export default function Hero() {
             language="rust"
             style={vscDarkPlus}
           >
-            {codeString}
+            {`${codeString.substring(0, subIndex)}${showBlink ? "|" : " "}`}
           </SyntaxHighlighter>
         </Macbook>
       </div>
